313db970cc40b889ef50585d30c057f5f6fd953b
deng19081994 <deng19081994@mail.ru>
2019-06-13 17:26:28 +0300

    task3209 solution(6)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task32.task3209;
+
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+    private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void init() {
+        initGui();
+        FrameListener frameListener = new FrameListener(this);
+        this.addWindowListener(frameListener);
+        setVisible(true);
+    }
+
+    public void initMenuBar() {
+
+    }
+
+    public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+
+        JScrollPane jScrollHtml = new JScrollPane(htmlTextPane);
+        JScrollPane jScrollTextPane = new JScrollPane(plainTextPane);
+
+        tabbedPane.addTab("HTML", jScrollHtml);
+        tabbedPane.addTab("Текст", jScrollTextPane);
+
+        tabbedPane.setPreferredSize(new Dimension(250, 300));
+
+        TabbedPaneChangeListener changeListener = new TabbedPaneChangeListener(this);
+        tabbedPane.addChangeListener(changeListener);
+
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
+
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void selectedTabChanged() {
+    }
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void exit() {
+        controller.exit();
+    }
+}
