1986e8ce050d7aa30b53162c8ff56e75509d843b
deng19081994 <deng19081994@mail.ru>
2019-06-18 21:28:39 +0300

    task 2712(3)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,12 +1,14 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
+
 public class Restaurant {
     public static void main(String[] args) {
-        Tablet tablet = new Tablet(1);
+        Cook cook = new Cook("Махмуд");
 
-        tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
+        Tablet tablet = new Tablet(1);
+        tablet.addObserver(cook);
+
         tablet.createOrder();
     }
 }
