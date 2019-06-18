8e143de3c73eeb5dec5a38f66d880015788d6f87
deng19081994 <deng19081994@mail.ru>
2019-06-18 18:39:41 +0300

    task 2712(2)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task27.task2712;
 
-import com.javarush.task.task27.task2712.kitchen.Dish;
-
 public class Restaurant {
     public static void main(String[] args) {
-        System.out.println(Dish.allDishesToString());
+        Tablet tablet = new Tablet(1);
+
+        tablet.createOrder();
+        tablet.createOrder();
+        tablet.createOrder();
+        tablet.createOrder();
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/kitchen/Order.java
@@ -1,14 +1,37 @@
 package com.javarush.task.task27.task2712.kitchen;
 
+import com.javarush.task.task27.task2712.ConsoleHelper;
 import com.javarush.task.task27.task2712.Tablet;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Order {
     private final Tablet tablet;
     protected List<Dish> dishes;
 
-    public Order(Tablet tablet) {
+    public Order(Tablet tablet) throws IOException {
         this.tablet = tablet;
+        this.dishes = ConsoleHelper.getAllDishesForOrder();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+
+        if (dishes.size() > 0) {
+            result.append("Your order: [");
+            for (Dish dish : dishes) {
+                result.append(dish + ", ");
+            }
+
+            int lastIndex = result.length();
+            result.deleteCharAt(--lastIndex);
+            result.deleteCharAt(--lastIndex);
+            result.append("] of ");
+            result.append(tablet.toString());
+        }
+
+        return result.toString();
     }
 }
