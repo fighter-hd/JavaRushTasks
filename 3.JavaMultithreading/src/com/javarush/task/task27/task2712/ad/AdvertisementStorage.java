e378d9688bcb3123bfc61aca68abd792a57f026c
deng19081994 <deng19081994@mail.ru>
2019-06-19 00:10:37 +0300

    task 2712(7)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -1,14 +1,50 @@
 package com.javarush.task.task27.task2712.ad;
 
-public class AdvertisementStorage {
-    private AdvertisementStorage() {
-    }
+import java.util.ArrayList;
+import java.util.List;
 
-    private static class AdvertisementStorageHolder {
-        private final static AdvertisementStorage instance = new AdvertisementStorage();
+public class AdvertisementStorage {
+    private final List<Advertisement> videos = new ArrayList<>();
+
+    private AdvertisementStorage() {
+        Object someContent = new Object();
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60)); //10 min
     }
 
     public static AdvertisementStorage getInstance() {
         return AdvertisementStorageHolder.instance;
     }
+
+    private static class AdvertisementStorageHolder {
+        private final static AdvertisementStorage instance = new AdvertisementStorage();
+
+    }
+
+    public List<Advertisement> list() {
+        return videos;
+    }
+
+    public void add(Advertisement advertisement) {
+        videos.add(advertisement);
+    }
+
+
+    //    private static volatile AdvertisementStorage instance;
+//
+//    private AdvertisementStorage() {
+//    }
+//
+//    public static AdvertisementStorage getInstance() {
+//        if (instance == null) {
+//            synchronized (AdvertisementStorage.class) {
+//                if (instance == null) {
+//                    instance = new AdvertisementStorage();
+//                }
+//            }
+//        }
+//
+//        return instance;
+//    }
 }
