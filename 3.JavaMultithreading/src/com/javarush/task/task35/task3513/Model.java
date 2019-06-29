a8f03a47f23b16da671e42550754aaea96101409
deng19081994 <deng19081994@mail.ru>
2019-06-29 23:29:53 +0300

    task3513(6)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -6,14 +6,17 @@ import java.util.List;
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles = gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
-    int maxTile = 0;
-    int score = 0;
+    int maxTile;
+    int score;
 
     public Model() {
         resetGameTiles();
     }
 
     void resetGameTiles() {
+        maxTile = 0;
+        score = 0;
+
         for (int i = 0; i < gameTiles.length; i++) {
             for (int j = 0; j < gameTiles[i].length; j++) {
                 gameTiles[i][j] = new Tile();
@@ -45,9 +48,11 @@ public class Model {
         }
     }
 
-    private void compressTiles(Tile[] tiles) {
+    private boolean compressTiles(Tile[] tiles) {
         List<Tile> emptyTiles = new ArrayList<>();
         List<Tile> nonEmptyTiles = new ArrayList<>();
+
+        boolean isChanged = false;
 
         for (Tile tile : tiles) {
             if (tile.isEmpty()) {
@@ -57,18 +62,37 @@ public class Model {
             }
         }
 
+        Tile[] newTiles = new Tile[tiles.length];
+
         for (int i = 0; i < nonEmptyTiles.size(); i++) {
-            tiles[i] = nonEmptyTiles.get(i);
+            newTiles[i] = nonEmptyTiles.get(i);
         }
 
         for (int i = 0; i < emptyTiles.size(); i++) {
-            tiles[i + nonEmptyTiles.size()] = emptyTiles.get(i);
+            newTiles[i + nonEmptyTiles.size()] = emptyTiles.get(i);
         }
+
+        for (int i = 0; i < tiles.length; i++) {
+            if (newTiles[i] != tiles[i]) {
+                isChanged = true;
+                break;
+            }
+        }
+
+        for (int i = 0; i < newTiles.length; i++) {
+            tiles[i] = newTiles[i];
+        }
+
+        return isChanged;
     }
 
-    private void mergeTiles(Tile[] tiles) {
+    private boolean mergeTiles(Tile[] tiles) {
+        boolean isChanged = false;
+
         for (int i = 0; i < tiles.length - 1; i++) {
-            if (tiles[i].value == tiles[i + 1].value) {
+            if (tiles[i].value == tiles[i + 1].value && tiles[i].value > 0) {
+                isChanged = true;
+
                 tiles[i].value *= 2;
                 tiles[i + 1].value = 0;
 
@@ -80,12 +104,24 @@ public class Model {
             }
         }
 
-        compressTiles(tiles);
+        return isChanged;
+    }
+
+    void left() {
+        boolean hasChange = false;
+        for (Tile[] tiles : gameTiles) {
+            if (mergeTiles(tiles) | compressTiles(tiles))
+                hasChange = true;
+        }
+
+        if (hasChange)
+            addTile();
     }
 
 //    public static void main(String[] args) {
 //        Model m = new Model();
 //
+////        m.gameTiles = new Tile[][]{{new Tile(0), new Tile(0), new Tile(0), new Tile(0)},
 //        m.gameTiles = new Tile[][]{{new Tile(4), new Tile(4), new Tile(2), new Tile(0)},
 //                                   {new Tile(4), new Tile(2), new Tile(0), new Tile(4)},
 //                                   {new Tile(4), new Tile(4), new Tile(4), new Tile(0)},
@@ -100,20 +136,29 @@ public class Model {
 //
 //        System.out.println("=========================================================================================");
 //
-//        m.mergeTiles(m.gameTiles[0]);
-//        m.mergeTiles(m.gameTiles[1]);
-//        m.mergeTiles(m.gameTiles[2]);
-//        m.mergeTiles(m.gameTiles[3]);
+//        m.left();
 //
-//        System.out.println("After merge");
 //        for (Tile[] tiles : m.gameTiles) {
 //            for (Tile tile : tiles) {
 //                System.out.print(tile.value + " ");
 //            }
 //            System.out.println();
 //        }
-//        System.out.println("=========================================================================================");
 //
+////        m.mergeTiles(m.gameTiles[0]);
+////        m.mergeTiles(m.gameTiles[1]);
+////        m.mergeTiles(m.gameTiles[2]);
+////        m.mergeTiles(m.gameTiles[3]);
+////
+////        System.out.println("After merge");
+////        for (Tile[] tiles : m.gameTiles) {
+////            for (Tile tile : tiles) {
+////                System.out.print(tile.value + " ");
+////            }
+////            System.out.println();
+////        }
+////        System.out.println("=========================================================================================");
+////
 ////        m.compressTiles(m.gameTiles[0]);
 ////        m.compressTiles(m.gameTiles[1]);
 ////        m.compressTiles(m.gameTiles[2]);
@@ -126,8 +171,8 @@ public class Model {
 ////            }
 ////            System.out.println();
 ////        }
-//
-//        System.out.println("\nScore = " + m.score);
-//        System.out.println("Max tile = " + m.maxTile);
+////
+////        System.out.println("\nScore = " + m.score);
+////        System.out.println("Max tile = " + m.maxTile);
 //    }
 }
