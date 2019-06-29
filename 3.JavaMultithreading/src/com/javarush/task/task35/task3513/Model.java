7c906a3d9b6387ca52b6d7fcfb820fdab01425ef
deng19081994 <deng19081994@mail.ru>
2019-06-29 15:26:01 +0300

    task3513(3)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,5 +1,14 @@
 package com.javarush.task.task35.task3513;
 
 public class Model {
+    private static final int FIELD_WIDTH = 4;
+    private Tile[][] gameTiles = gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
 
+    public Model() {
+        for (int i = 0; i < gameTiles.length; i++) {
+            for (int j = 0; j < gameTiles[i].length; j++) {
+                gameTiles[i][j] = new Tile();
+            }
+        }
+    }
 }
