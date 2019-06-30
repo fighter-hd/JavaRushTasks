fc60567771763c6c60fe253cd50076c90f476286
deng19081994 <deng19081994@mail.ru>
2019-06-30 12:47:40 +0300

    task3513(10)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Controller.java
@@ -54,4 +54,8 @@ public class Controller extends KeyAdapter {
     int getScore() {
         return model.score;
     }
+
+    public View getView() {
+        return view;
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Main.java
@@ -1,7 +1,19 @@
 package com.javarush.task.task35.task3513;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+        Model model = new Model();
+        Controller controller = new Controller(model);
+        JFrame gameJFrame = new JFrame();
 
+        gameJFrame.setTitle("2048");
+        gameJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        gameJFrame.setSize(450,500);
+        gameJFrame.setResizable(false);
+        gameJFrame.add(controller.getView());
+        gameJFrame.setLocationRelativeTo(null);
+        gameJFrame.setVisible(true);
     }
 }
