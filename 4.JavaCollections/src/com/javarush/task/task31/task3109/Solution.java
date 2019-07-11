dd307fd2500ba4a57d4181ab51ff6f7239d8bb14
deng19081994 <deng19081994@mail.ru>
2019-07-11 17:31:57 +0300

    task3109

---
+++ 4.JavaCollections/src/com/javarush/task/task31/task3109/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task31.task3109;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.Properties;
+
+/* 
+Читаем конфиги
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        Properties properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.xml");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/properties.txt");
+        properties.list(System.out);
+
+        properties = solution.getProperties("4.JavaCollections/src/com/javarush/task/task31/task3109/notExists");
+        properties.list(System.out);
+    }
+
+    public Properties getProperties(String fileName) {
+        Properties properties = new Properties();
+        try {
+            if (fileName.endsWith(".xml")) {
+                properties.loadFromXML(new FileInputStream(fileName));
+
+            } else if (fileName.endsWith(".txt") || fileName.endsWith(".properties")) {
+                properties.load(new FileReader(fileName));
+
+            } else {
+                properties.load(new FileInputStream(fileName));
+            }
+
+            return properties;
+
+        } catch (Throwable throwable) {
+            return properties;
+        }
+    }
+}
