f48d250ca426775f6b00c547d257060aaecf73c8
deng19081994 <deng19081994@mail.ru>
2019-07-15 00:08:10 +0300

    task2028(4)

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,21 +1,145 @@
 package com.javarush.task.task20.task2028;
 
 import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /*
 Построй дерево(1)
 */
 public class CustomTree extends AbstractList<String> implements Cloneable, Serializable {
+    Map<Integer, List<Entry<String>>>  tree = new TreeMap<>();
+    Entry<String> root;
+    private int size;
+
+    public CustomTree() {
+        root = new Entry<>("theRoot");
+    }
+
+    @Override
+    public boolean add(String s) {
+        if (root.isAvailableToAddChildren()) {
+            addFirstDeepElements(s);
+
+        } else {
+            Entry<String> newElement = new Entry<>(s);
+            int deep = 2;
+
+            while (true) {
+                if (isAvailableToAddOnCurrentDeep(deep)) {
+                    List<Entry<String>> currentParentsDeep = tree.get(deep - 1);
+
+                    for (Entry<String> currentParent : currentParentsDeep) {
+                        if (currentParent.isAvailableToAddChildren()) {
+                            List<Entry<String>> childDeepList = tree.get(1);
+
+                            if (childDeepList == null) {
+                                childDeepList = new ArrayList<>();
+                                tree.put(deep, childDeepList);
+                            }
+
+                            if (currentParent.availableToAddLeftChildren) {
+                                addLeftChild(currentParent, newElement, childDeepList);
+
+                            } else {
+                                addRightChild(currentParent, newElement, childDeepList);
+                            }
+
+                            break;
+                        }
+                    }
+
+                    break;
+
+                } else {
+                    deep++;
+                }
+            }
+        }
+
+        size++;
+
+        return true;
+    }
+
+    private void addFirstDeepElements(String s) {
+        List<Entry<String>> firstDeepList = tree.computeIfAbsent(1, k -> new ArrayList<>());
+
+        Entry<String> newElement = new Entry<>(s);
+
+        if (root.availableToAddLeftChildren) {
+            addLeftChild(root, newElement, firstDeepList);
+
+        } else {
+            addRightChild(root, newElement, firstDeepList);
+        }
+    }
+
+    private void addLeftChild(Entry<String> parent, Entry<String> child, List<Entry<String>> childDeepList) {
+        child.parent = parent;
+        parent.leftChild = child;
+        parent.availableToAddLeftChildren = false;
+        childDeepList.add(child);
+    }
+
+    private void addRightChild (Entry<String> parent, Entry<String> child, List<Entry<String>> childDeepList) {
+        child.parent = parent;
+        parent.rightChild = child;
+        parent.availableToAddRightChildren = false;
+        childDeepList.add(child);
+    }
+
+    private boolean isAvailableToAddOnCurrentDeep(int deep) {
+        List<Entry<String>> currentDeepList = tree.get(deep - 1);
+
+        for (Entry<String> element : currentDeepList) {
+            if (element.isAvailableToAddChildren()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public String getParent(String s) {
+        Entry<String> element = getElement(s, root);
+
+        if (element != null) {
+            return element.parent.elementName;
+        } else {
+            return null;
+        }
+    }
+
+    private Entry<String> getElement(String checkedName, Entry<String> currentEntry) {
+        if (currentEntry.elementName.equals(checkedName)) {
+            return currentEntry;
+        }
+
+        Entry<String> childElement = null;
+
+        if (currentEntry.leftChild != null) {
+            childElement = getElement(checkedName, currentEntry.leftChild);
+        }
+
+        if (childElement == null && currentEntry.rightChild != null) {
+            childElement = getElement(checkedName, currentEntry.rightChild);
+        }
+
+        return childElement;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
     static class Entry<T> implements Serializable {
         String elementName;
-        boolean availableToAddLeftChildren;
-        boolean availableToAddRightChildren;
-        Entry<T> parent;
         Entry<T> leftChild;
         Entry<T> rightChild;
+        Entry<T> parent;
+        boolean availableToAddLeftChildren;
+        boolean availableToAddRightChildren;
 
         public Entry(String elementName) {
             this.elementName = elementName;
@@ -25,6 +149,11 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
 
         public boolean isAvailableToAddChildren() {
             return availableToAddLeftChildren || availableToAddRightChildren;
+        }
+
+        @Override
+        public String toString() {
+            return elementName;
         }
     }
 
@@ -61,10 +190,5 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     @Override
     protected void removeRange(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public int size() {
-        return 0;
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -5,5 +5,28 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         List<String> list = new CustomTree();
+
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+
+//        System.out.println("The list size is " + list.size());
+//        System.out.println("The expected parent is 3. The actual parent is " + ((CustomTree) list).getParent("8"));
+//        System.out.println("The expected parent is null. The actual parent is " + ((CustomTree) list).getParent("20"));
+        System.out.println("For 3 parent is " + ((CustomTree) list).getParent("3"));
+        System.out.println("For 4 parent is " + ((CustomTree) list).getParent("4"));
+        System.out.println("For 5 parent is " + ((CustomTree) list).getParent("5"));
+        System.out.println("For 6 parent is " + ((CustomTree) list).getParent("6"));
+        System.out.println("For 7 parent is " + ((CustomTree) list).getParent("7"));
+        System.out.println("For 8 parent is " + ((CustomTree) list).getParent("8"));
+        System.out.println("For 9 parent is " + ((CustomTree) list).getParent("9"));
+        System.out.println("For 10 parent is " + ((CustomTree) list).getParent("10"));
+        System.out.println("For 11 parent is " + ((CustomTree) list).getParent("11"));
+        System.out.println("For 12 parent is " + ((CustomTree) list).getParent("12"));
+        System.out.println("For 13 parent is " + ((CustomTree) list).getParent("13"));
+        System.out.println("For 14 parent is " + ((CustomTree) list).getParent("14"));
+        System.out.println("For 15 parent is " + ((CustomTree) list).getParent("15"));
+        System.out.println("For 16 parent is " + ((CustomTree) list).getParent("16"));
+        System.out.println("For 20 parent is " + ((CustomTree) list).getParent("20"));
     }
 }
