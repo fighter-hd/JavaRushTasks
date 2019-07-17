038430538f10f538d2a412f47e6ea205d559466a
deng19081994 <deng19081994@mail.ru>
2019-07-17 18:48:38 +0300

    task3208

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3208/Solution.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task32.task3208;
+
+import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+/* 
+RMI-2
+*/
+public class Solution {
+    public static Registry registry;
+
+    // Pretend we're starting an RMI client as the CLIENT_THREAD thread
+    public static Thread CLIENT_THREAD = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+                for (String bindingName : registry.list()) {
+                    Animal service = (Animal) registry.lookup(bindingName);
+                    service.printName();
+                    service.speak();
+                }
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            } catch (NotBoundException e) {
+                e.printStackTrace();
+            }
+        }
+    });
+
+    // Pretend we're starting an RMI server as the SERVER_THREAD thread
+    public static Thread SERVER_THREAD = new Thread(() -> {
+        try {
+            registry = LocateRegistry.createRegistry(2099);
+
+            final Animal dog = new Dog("Pyos");
+            final Animal cat = new Cat("Cote");
+
+            Remote stubDog = UnicastRemoteObject.exportObject(dog, 0);
+            Remote stubCat = UnicastRemoteObject.exportObject(cat, 0);
+
+            registry.bind("dog", stubDog);
+            registry.bind("cat", stubCat);
+
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (AlreadyBoundException e) {
+            e.printStackTrace();
+        }
+    });
+
+    public static void main(String[] args) throws InterruptedException {
+        // Starting an RMI server thread
+        SERVER_THREAD.start();
+        Thread.sleep(1000);
+        // Start the client
+        CLIENT_THREAD.start();
+    }
+}
