078606ed0eb2db93499d1b565a7cd93dd1210b4c
deng19081994 <deng19081994@mail.ru>
2019-07-21 08:22:20 +0300

    task3305

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3305/Vehicle.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task33.task3305;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS,  property="className")
+public abstract class Vehicle {
+    protected String name;
+    protected String owner;
+    protected int age;
+}
