70947cdc1daeb9d6c432d0e042137f8edeb7a7d8
deng19081994 <deng19081994@mail.ru>
2019-07-29 22:12:51 +0300

    task3402

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3402/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task34.task3402;
+
+/* 
+Факториал с помощью рекурсии
+*/
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.factorial(9));     //362880
+        System.out.println(solution.factorial(0));     //1
+        System.out.println(solution.factorial(1));     //1
+    }
+
+    public int factorial(int n) {
+        int result;
+
+        if (n > 0) {
+            result = n * factorial(n - 1);
+        } else {
+            result = 1;
+        }
+
+        return result;
+    }
+}
