3332d57fa6cef6105f80ebd2c4899f682384bd4e
deng19081994 <deng19081994@mail.ru>
2019-07-29 15:08:32 +0300

    task2515(5)

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -1,5 +1,43 @@
 package com.javarush.task.task25.task2515;
 
 public abstract class BaseObject {
+    private double x;
+    private double y;
+    private double radius;
+    private boolean isAlive;
 
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        isAlive = true;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
 }
