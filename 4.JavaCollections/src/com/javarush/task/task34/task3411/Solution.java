5977e546b72610242db87c30a301fc3597d47d63
deng19081994 <deng19081994@mail.ru>
2019-08-08 19:24:24 +0300

    task3411

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3411/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task34.task3411;
+
+/* 
+Ханойские башни
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int numRings = 3;
+        moveRing('A', 'B', 'C', numRings);
+    }
+
+    public static void moveRing(char a, char b, char c, int numRings) {
+        if (numRings > 0) {
+            moveRing(a, c, b, numRings - 1);
+            System.out.println("from " + a + " to " + b);
+            moveRing(c, b, a, numRings - 1);
+        }
+    }
+}
