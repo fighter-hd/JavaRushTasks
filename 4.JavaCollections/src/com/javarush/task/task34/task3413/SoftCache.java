0f499596e574767ef3dd6ea4c08a31af682fb3c9
deng19081994 <deng19081994@mail.ru>
2019-08-26 17:43:08 +0300

    task3413

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3413/SoftCache.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task34.task3413;
+
+import java.lang.ref.SoftReference;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SoftCache {
+    private Map<Long, SoftReference<AnyObject>> cacheMap = new ConcurrentHashMap<>();
+
+    public AnyObject get(Long key) {
+        SoftReference<AnyObject> softReference = cacheMap.get(key);
+
+        if (softReference != null) {
+            return softReference.get();
+
+        } else {
+            return null;
+        }
+    }
+
+    public AnyObject put(Long key, AnyObject value) {
+        SoftReference<AnyObject> softReference = cacheMap.put(key, new SoftReference<>(value));
+
+        if (softReference != null) {
+            AnyObject removedObject = softReference.get();
+            softReference.clear();
+            return removedObject;
+
+        } else {
+            return null;
+        }
+    }
+
+    public AnyObject remove(Long key) {
+        SoftReference<AnyObject> softReference = cacheMap.remove(key);
+
+        if (softReference != null) {
+            AnyObject removedObject = softReference.get();
+            softReference.clear();
+            return removedObject;
+
+        } else {
+            return null;
+        }
+    }
+}
