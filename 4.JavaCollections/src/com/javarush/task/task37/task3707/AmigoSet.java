6163b863ff67d1784ccf57af6980eb0f480ee925
deng19081994 <deng19081994@mail.ru>
2019-09-03 16:51:12 +0300

    task3707(3)

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -21,17 +21,37 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
     }
 
     @Override
+    public boolean isEmpty() {
+        return this.map.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return this.map.size();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return this.map.containsKey(o);
+    }
+
+    @Override
     public boolean add(E e) {
         return this.map.put(e, PRESENT) == null;
     }
 
     @Override
-    public Iterator<E> iterator() {
-        return null;
+    public boolean remove(Object o) {
+        return this.map.remove(o) == o;
     }
 
     @Override
-    public int size() {
-        return 0;
+    public Iterator<E> iterator() {
+        return this.map.keySet().iterator();
+    }
+
+    @Override
+    public void clear() {
+        this.map.clear();
     }
 }
