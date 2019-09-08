6784f9d302763b4339b5bfc8edb2df51de7d0768
deng19081994 <deng19081994@mail.ru>
2019-09-08 18:22:33 +0300

    task3505

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3505/ConvertableUtil.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task35.task3505;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ConvertableUtil {
+
+    public static <K, V extends Convertable<K>> Map<K, V> convert(List<V> list) {
+        Map<K, V> result = new HashMap<>();
+
+        for (V value : list) {
+            result.put(value.getKey(), value);
+        }
+
+        return result;
+    }
+}
