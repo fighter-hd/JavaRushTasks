aae46119a744699588ccc8c14bcc3b9926bc245c
deng19081994 <deng19081994@mail.ru>
2019-09-10 13:07:13 +0300

    task3702(4.1)

--- 4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task37.task3702;
 
 import com.javarush.task.task37.task3702.female.FemaleFactory;
-import com.javarush.task.task37.task3702.female.Woman;
 import com.javarush.task.task37.task3702.male.MaleFactory;
 
 public class FactoryProducer {
