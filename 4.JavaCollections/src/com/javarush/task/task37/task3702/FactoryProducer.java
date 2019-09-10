45c6ad87a47a1229b18aa033d60db7dc666fdb14
deng19081994 <deng19081994@mail.ru>
2019-09-10 13:03:25 +0300

    task3702(4)

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task37.task3702;
+
+import com.javarush.task.task37.task3702.female.FemaleFactory;
+import com.javarush.task.task37.task3702.female.Woman;
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
+public class FactoryProducer {
+    public enum HumanFactoryType {
+        MALE,
+        FEMALE
+    }
+
+    public static AbstractFactory getFactory(HumanFactoryType type) {
+        if (type == HumanFactoryType.MALE) {
+            return new MaleFactory();
+
+        } else if (type == HumanFactoryType.FEMALE) {
+            return new FemaleFactory();
+        }
+
+        return null;
+    }
+}
