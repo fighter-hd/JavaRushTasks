5de2f8670ba9ab742133671465f927a811da8a40
deng19081994 <deng19081994@mail.ru>
2019-09-22 14:06:54 +0300

    task3310(1)

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task33.task3310;
+
+public class Shortener {
+
+    public Long getId(String string) {
+        return null;
+    }
+
+    public String getString(Long id) {
+        return null;
+    }
+}
