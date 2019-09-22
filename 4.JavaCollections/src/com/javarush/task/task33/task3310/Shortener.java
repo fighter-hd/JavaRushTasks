833242b360bb348e6d882f355388aa67ec63c565
deng19081994 <deng19081994@mail.ru>
2019-09-22 14:45:09 +0300

    task3310(3)

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,12 +1,27 @@
 package com.javarush.task.task33.task3310;
 
-public class Shortener {
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
 
-    public Long getId(String string) {
-        return null;
+public class Shortener {
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id) {
-        return null;
+    public synchronized Long getId(String string) {
+        if (this.storageStrategy.containsValue(string)) {
+            return this.storageStrategy.getKey(string);
+
+        } else {
+            this.lastId++;
+            storageStrategy.put(this.lastId, string);
+            return this.lastId;
+        }
+    }
+
+    public synchronized String getString(Long id) {
+        return this.storageStrategy.getValue(id);
     }
 }
