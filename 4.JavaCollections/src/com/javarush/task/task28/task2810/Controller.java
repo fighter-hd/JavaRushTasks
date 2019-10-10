a6078a0578a941d2569369adf4d85bd79fed8835
deng19081994 <deng19081994@mail.ru>
2019-10-10 14:57:47 +0300

    task2810(2)

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task28.task2810;
+
+import com.javarush.task.task28.task2810.model.Provider;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Controller {
+    private Provider[] providers;
+
+    public Controller(Provider... providers) {
+        if (providers != null && providers.length != 0) {
+            this.providers = providers.clone();
+
+        } else {
+            throw new IllegalArgumentException("Adding less than one provider is invalid operation");
+        }
+        this.providers = providers;
+    }
+
+    @Override
+    public String toString() {
+        return "Controller{" +
+                "providers=" + Arrays.toString(providers) +
+                '}';
+    }
+}
