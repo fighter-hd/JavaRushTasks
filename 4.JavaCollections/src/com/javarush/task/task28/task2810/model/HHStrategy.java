cc200b7153edffda4dbc87954592204fb337b64b
deng19081994 <deng19081994@mail.ru>
2019-10-11 13:10:54 +0300

    task2810(7)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -14,7 +14,10 @@ public class HHStrategy implements Strategy {
     @Override
     public List<Vacancy> getVacancies(String searchString) {
         try {
-            Document document = Jsoup.connect(String.format(URL_FORMAT, "Kiev", 1)).get();
+            Document document = Jsoup.connect(String.format(URL_FORMAT, "Kiev", 1))
+                    .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36")
+                    .referrer("")
+                    .get();
 
         } catch (IOException e) {
             e.printStackTrace();
