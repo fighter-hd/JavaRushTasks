074b4592790c8e04b3e40b6ad6829f49e16f81d3
deng19081994 <deng19081994@mail.ru>
2019-10-11 12:46:35 +0300

    task2810(6)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/vo/Vacancy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/vo/Vacancy.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task28.task2810.vo;
 
+import java.util.Objects;
+
 public class Vacancy {
     private String title;
     private String salary;
@@ -8,7 +10,23 @@ public class Vacancy {
     private String siteName;
     private String url;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Vacancy)) return false;
+        Vacancy vacancy = (Vacancy) o;
+        return Objects.equals(getTitle(), vacancy.getTitle()) &&
+                Objects.equals(getSalary(), vacancy.getSalary()) &&
+                Objects.equals(getCity(), vacancy.getCity()) &&
+                Objects.equals(getCompanyName(), vacancy.getCompanyName()) &&
+                Objects.equals(getSiteName(), vacancy.getSiteName()) &&
+                Objects.equals(getUrl(), vacancy.getUrl());
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getSalary(), getCity(), getCompanyName(), getSiteName(), getUrl());
+    }
 
     public String getTitle() {
         return title;
