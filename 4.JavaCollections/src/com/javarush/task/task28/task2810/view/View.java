ff21756505ec53375c82104be606de2ebfdb25f8
deng19081994 <deng19081994@mail.ru>
2019-10-13 20:59:35 +0300

    task2810(9)

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/View.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task28.task2810.view;
+
+import com.javarush.task.task28.task2810.Controller;
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+import java.util.List;
+
+public interface View {
+    void update(List<Vacancy> vacancies);
+    void setController(Controller controller);
+}
