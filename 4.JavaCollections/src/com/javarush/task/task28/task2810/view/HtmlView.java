17d02327cd017d85acb27f37064d984a6dc1b889
deng19081994 <deng19081994@mail.ru>
2019-10-14 11:37:23 +0300

    task2810(13)

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/view/HtmlView.java
@@ -7,10 +7,26 @@ import java.util.List;
 
 public class HtmlView implements View {
     private Controller controller;
+    private final String filePath = "./4.JavaCollections/src/"
+                                    + this.getClass().getPackage().getName().replace('.', '/')
+                                    + "/vacancies.html";
 
     @Override
     public void update(List<Vacancy> vacancies) {
-        System.out.println(vacancies.size());
+        try {
+            String stringContent = getUpdatedFileContent(vacancies);
+            updateFile(stringContent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getUpdatedFileContent(List<Vacancy> vacancyList) {
+        return null;
+    }
+
+    private void updateFile(String stringContent) {
+
     }
 
     @Override
