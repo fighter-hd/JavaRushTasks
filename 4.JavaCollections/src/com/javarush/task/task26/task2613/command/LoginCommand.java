4b983b92d1f37a3b6acc20e0c92a4b7a93f8a2dc
deng19081994 <deng19081994@mail.ru>
2019-10-28 00:19:19 +0200

    task2613(12)

--- 4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
+++ 4.JavaCollections/src/com/javarush/task/task26/task2613/command/LoginCommand.java
@@ -1,14 +1,17 @@
 package com.javarush.task.task26.task2613.command;
 
+import com.javarush.task.task26.task2613.CashMachine;
 import com.javarush.task.task26.task2613.ConsoleHelper;
 import com.javarush.task.task26.task2613.exception.InterruptOperationException;
 
+import java.util.ResourceBundle;
+
 public class LoginCommand implements Command {
+    private ResourceBundle validCreditCards = ResourceBundle.getBundle(CashMachine.class.getPackage().getName()
+                                                                        + ".resources.verifiedCards");
+
     @Override
     public void execute() throws InterruptOperationException {
-        String hardCardNumber = "123456789012";
-        String hardPin = "1234";
-
         ConsoleHelper.writeMessage("Enter card number - 12 digit.");
         String cardNumber = ConsoleHelper.readString();
 
@@ -25,19 +28,20 @@ public class LoginCommand implements Command {
                 pin = ConsoleHelper.readString();
 
             } else {
-                if ( ! cardNumber.equals(hardCardNumber)) {
-                    ConsoleHelper.writeMessage("Enter hard card number - 123456789012");
+                if ( ! this.validCreditCards.containsKey(cardNumber)) {
+                    ConsoleHelper.writeMessage("Card number is not found. Try again:");
                     cardNumber = ConsoleHelper.readString();
                     continue;
 
-                } else if (! pin.equals(hardPin)) {
-                    ConsoleHelper.writeMessage("Enter hard card pin code - 1234");
+                }
+
+                if ( ! pin.equals(this.validCreditCards.getString(cardNumber) ) ) {
+                    ConsoleHelper.writeMessage("Pin code is wrong. Enter pin code again:");
                     pin = ConsoleHelper.readString();
                     continue;
-
-                } else {
-                    ConsoleHelper.writeMessage("Verification success.");
                 }
+
+                ConsoleHelper.writeMessage("Verification success.");
                 break;
             }
         }
