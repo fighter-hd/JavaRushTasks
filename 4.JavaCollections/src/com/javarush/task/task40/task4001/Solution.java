921bc6ca71686ed6e257fde8fa702a53e321aa79
deng19081994 <deng19081994@mail.ru>
2019-10-29 12:35:01 +0200

    task4001

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4001/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task40.task4001;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/* 
+POST, а не GET
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.sendPost(new URL("http://requestb.in/1cse9qt1"), "name=zapp&mood=good&locale=&id=777");
+    }
+
+    public void sendPost(URL url, String urlParameters) throws Exception {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+        connection.setDoOutput(true);
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+        bufferedWriter.write(urlParameters);
+        bufferedWriter.flush();
+        bufferedWriter.close();
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String responseLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((responseLine = bufferedReader.readLine()) != null) {
+            response.append(responseLine);
+        }
+        bufferedReader.close();
+
+        System.out.println("Response: " + response.toString());
+    }
+}
