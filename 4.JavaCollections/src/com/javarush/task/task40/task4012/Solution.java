35994c515b0141badb50e5ffbad3f54cad2b861e
deng19081994 <deng19081994@mail.ru>
2019-10-31 21:17:51 +0200

    task4012

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4012/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task40.task4012;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+/* 
+Полезные методы DateTime API
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isLeap(LocalDate date) {
+        return date.isLeapYear();
+    }
+
+    public static boolean isBefore(LocalDateTime dateTime) {
+        return dateTime.isBefore(LocalDateTime.now());
+    }
+
+    public static LocalTime addTime(LocalTime time, int n, ChronoUnit chronoUnit) {
+        return time.plus(n, chronoUnit);
+    }
+
+    public static Period getPeriodBetween(LocalDate firstDate, LocalDate secondDate) {
+        LocalDate beforeDate;
+        LocalDate afterDate;
+
+        if (firstDate.isBefore(secondDate)) {
+            beforeDate = firstDate;
+            afterDate = secondDate;
+
+        } else {
+            afterDate = firstDate;
+            beforeDate = secondDate;
+        }
+
+        return Period.between(beforeDate, afterDate);
+    }
+}
