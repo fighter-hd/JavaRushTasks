8111a0472a2511e1e2404f6261a22a733a695f00
deng19081994 <deng19081994@mail.ru>
2019-11-01 17:36:46 +0200

    task3410(9)

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,5 +1,6 @@
 package com.javarush.task.task34.task3410.view;
 
+import com.javarush.task.task34.task3410.controller.EventListener;
 import com.javarush.task.task34.task3410.model.Box;
 import com.javarush.task.task34.task3410.model.Home;
 import com.javarush.task.task34.task3410.model.Player;
@@ -10,6 +11,7 @@ import java.awt.*;
 
 public class Field extends JPanel {
     private View view;
+    private EventListener eventListener;
 
     public Field(View view) {
         this.view = view;
@@ -22,5 +24,9 @@ public class Field extends JPanel {
         new Box(200, 250).draw(g);
         new Home(200, 100).draw(g);
         new Wall(250, 300).draw(g);
+    }
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
     }
 }
