2d7851003fd21790134f49cfc36a2f31e8c3002a
deng19081994 <deng19081994@mail.ru>
2019-11-01 17:29:46 +0200

    task3410(8)

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/GameObjects.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task34.task3410.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GameObjects {
+    private Set<Wall> walls;
+    private Set<Box> boxes;
+    private Set<Home> homes;
+    private Player player;
+
+    public GameObjects(Set<Wall> walls, Set<Box> boxes, Set<Home> homes, Player player) {
+        this.walls = walls;
+        this.boxes = boxes;
+        this.homes = homes;
+        this.player = player;
+    }
+
+    public Set<GameObject> getAll() {
+        Set<GameObject> allObjects = new HashSet<>();
+
+        allObjects.addAll(this.walls);
+        allObjects.addAll(this.boxes);
+        allObjects.addAll(this.homes);
+        allObjects.add(this.player);
+
+        return allObjects;
+    }
+
+    public Set<Wall> getWalls() {
+        return walls;
+    }
+
+    public Set<Box> getBoxes() {
+        return boxes;
+    }
+
+    public Set<Home> getHomes() {
+        return homes;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+}
